c75e18a64da70fad318992af6a4f99b6c19d3068
Khoirul Ansori <khoirulansori407@gmail.com>
2025-01-31 09:12:42 +0700

    hapus folder PERPUSTAKAAN

--- PERPUSTAKAAN/rak.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO "rak" ("no_rak", "kategori_rak") VALUES ('R21', 'Data Base');
-INSERT INTO "rak" ("no_rak", "kategori_rak") VALUES ('R22', 'Perangkat Lunak');
--- PERPUSTAKAAN/staf.sql
+++
@@ -1,1 +0,0 @@
-INSERT INTO "staf" ("id_staf", "nama", "jabatan", "alamat", "no_telp") VALUES ('St001', NULL, NULL, NULL, NULL);
